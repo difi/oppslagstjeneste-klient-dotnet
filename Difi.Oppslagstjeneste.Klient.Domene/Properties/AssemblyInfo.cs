cc9b8efc5cbf6793314dcb2618daf0d886873c0a
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-17 18:07:24 +0200

    Lagt til sharedassemblyinfo

--- Difi.Oppslagstjeneste.Klient.Domene/Properties/AssemblyInfo.cs
+++ Difi.Oppslagstjeneste.Klient.Domene/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91aab4dd-d6ca-4a0e-961b-c758c8b3171f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
