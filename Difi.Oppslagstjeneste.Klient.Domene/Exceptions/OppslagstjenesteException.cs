b506200e6ad4ce3a0e411fd1dc8aeec3ae9ed67c
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-22 14:19:56 +0200

    Lagt til exceptionklasser for xmlparsing og refaktorert HentPrintsertifikatSvar

---
+++ Difi.Oppslagstjeneste.Klient.Domene/Exceptions/OppslagstjenesteException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Difi.Oppslagstjeneste.Klient.Domene.Exceptions
+{
+    public abstract class OppslagstjenesteException : Exception
+    {
+        protected OppslagstjenesteException()
+        {
+        }
+
+        protected OppslagstjenesteException(string message)
+            : base(message)
+        {
+        }
+
+        protected OppslagstjenesteException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+    }
+}
--- Difi.Oppslagstjeneste.Klient/HentPrintSertifikatSvar.cs
+++ Difi.Oppslagstjeneste.Klient/HentPrintSertifikatSvar.cs
@@ -1,29 +1,58 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml;
+using Difi.Oppslagstjeneste.Klient.Domene.Exceptions;
 using Difi.Oppslagstjeneste.Klient.Felles.Envelope;
 
 namespace Difi.Oppslagstjeneste.Klient
 {
+    /// <summary>
+    /// Svar fra Oppslagstjeneste som inneholder sertifikat til printleverandør og adressen til leverandøren
+    /// av postkassetjenesten.
+    /// </summary>
     public class HentPrintSertifikatSvar
     {
+        private XmlNamespaceManager namespaceManager;
+
         public HentPrintSertifikatSvar(XmlDocument xmlDocument)
         {
-            var nsmgr = new XmlNamespaceManager(xmlDocument.NameTable);
-            nsmgr.AddNamespace("env", Navnerom.env11);
-            nsmgr.AddNamespace("ns", Navnerom.krr);
-            nsmgr.AddNamespace("difi", Navnerom.difi);
-
-            var personElements = xmlDocument.SelectSingleNode("/env:Envelope/env:Body/ns:HentPrintSertifikatRespons", nsmgr);
-
-            this.PostkasseleverandørAdresse = personElements.SelectSingleNode("./ns:postkasseleverandoerAdresse", nsmgr).InnerText;
-            this.Sertifikat = new X509Certificate2(Convert.FromBase64String(personElements.SelectSingleNode("./ns:X509Sertifikat", nsmgr).InnerText));
+            InitalizeNamespaceManager(xmlDocument);
+            ParseToClassMembers(xmlDocument);
         }
 
+        private void InitalizeNamespaceManager(XmlDocument xmlDocument)
+        {
+            namespaceManager = new XmlNamespaceManager(xmlDocument.NameTable);
+            namespaceManager.AddNamespace("env", Navnerom.env11);
+            namespaceManager.AddNamespace("ns", Navnerom.krr);
+            namespaceManager.AddNamespace("difi", Navnerom.difi);
+        }
+
+        private void ParseToClassMembers(XmlDocument xmlDocument)
+        {
+            try
+            {
+                var personElements = xmlDocument.SelectSingleNode(
+                    "/env:Envelope/env:Body/ns:HentPrintSertifikatRespons", namespaceManager);
+
+                PostkasseleverandørAdresse =
+                    personElements.SelectSingleNode("./ns:postkasseleverandoerAdresse", namespaceManager).InnerText;
+                Printsertifikat = new X509Certificate2(
+                    Convert.FromBase64String(
+                        personElements.SelectSingleNode("./ns:X509Sertifikat", namespaceManager).InnerText));
+            }
+            catch (Exception e)
+            {
+                throw new XmlParseException("Klarte ikke å parse svar fra Oppslagstjenesten.", e);
+            }
+        }
+
+
+        /// <summary>
         /// <summary>
         /// Et X509 Sertifikat.
         /// </summary>
-        public X509Certificate2 Sertifikat { get; set; }
+        public X509Certificate2 Printsertifikat { get; set; }
 
         /// <summary>
         /// Adresse til en leverandør av Postkassetjeneste
