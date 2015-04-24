64d9dbf31bb2d5a823de1beeb3e3c89132782578
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-24 13:06:31 +0200

    Startet på SoapException

---
+++ Difi.Oppslagstjeneste.Klient.Domene/Exceptions/SoapException.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+using Difi.Oppslagstjeneste.Klient.Felles.Envelope;
+
+namespace Difi.Oppslagstjeneste.Klient.Domene.Exceptions
+{
+    public class SoapException : OppslagstjenesteException
+    {
+
+        internal SoapException(string outerXml)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(outerXml);
+
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xmlDocument.NameTable);
+            namespaceManager.AddNamespace("env", Navnerom.env);
+            namespaceManager.AddNamespace("eb", Navnerom.eb);
+            
+             try
+            {
+                var rot = xmlDocument.DocumentElement;
+                var xPath = "//ns6:MessageId"; //Endre til passende
+                var targetNode = rot.SelectSingleNode(xPath, namespaceManager);
+
+        }
+    }
+}
