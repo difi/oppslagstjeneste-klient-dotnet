3f337ed15840eedf3a0f930d9569d01a70713714
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-28 14:18:11 +0200

    Splittet opp tester for Person

--- Difi.Oppslagstjeneste.Klient.Tester/Domene/PersonTester.cs
+++ Difi.Oppslagstjeneste.Klient.Tester/Domene/PersonTester.cs
@@ -10,43 +10,76 @@ namespace Difi.Oppslagstjeneste.Klient.Tester.Domene
     [TestClass]
     public class PersonTester
     {
+        private static Person _person;
+
         /// <summary>
         /// Tester oppretting av personobjekt fra Xml. Når dette objektet testes blir også følgende klasser
         /// implisitt testet: Epostadresse, Kontaktinformasjon, Mobiltelefonnummer og Sikkerdigitalpostadresse.
         /// </summary>
-        [TestMethod]
-        public void OpprettPersonFraXmlSuksess()
+        [ClassInitialize]
+        public static void OpprettPersonFraXmlSuksess(TestContext context)
         {
             var doc = new XmlDocument();
             doc.LoadXml(PersonXml());
             
-            Person p = null;
             try
             {
-                p = new Person(doc.DocumentElement);
+                _person = new Person(doc.DocumentElement);
             }
             catch { Assert.Fail(); }
-
-            Assert.AreEqual("08077000292", p.Personidentifikator);
-            Assert.IsFalse((bool)p.Reservasjon);
-            Assert.AreEqual(Tilstand.AKTIV, p.Status);
-
-            Assert.AreEqual("+4740485641", p.Kontaktinformasjon.Mobiltelefonnummer.Nummer);
-            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 19), p.Kontaktinformasjon.Mobiltelefonnummer.SistOppdatert);
-            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 19), p.Kontaktinformasjon.Mobiltelefonnummer.SistVerifisert);
-
-
-            Assert.AreEqual("extfjo@vegvesen.no", p.Kontaktinformasjon.Epostadresse.Epost);
-            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 30), p.Kontaktinformasjon.Epostadresse.SistOppdatert);
-            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 30), p.Kontaktinformasjon.Epostadresse.SistVerifisert);
-
-            Assert.AreEqual("id.porten.testuser#2346", p.SikkerDigitalPostAdresse.Postkasseadresse);
-            Assert.AreEqual("984661185", p.SikkerDigitalPostAdresse.PostkasseleverandørAdresse);
-
-            var certificateData = Convert.FromBase64String(@"MIIE7jCCA9agAwIBAgIKGBZrmEgzTHzeJjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQGEwJOTzEdMBsGA1UECgwUQnV5cGFzcyBBUy05ODMxNjMzMjcxIzAhBgNVBAMMGkJ1eXBhc3MgQ2xhc3MgMyBUZXN0NCBDQSAzMB4XDTE0MDQyNDEyMzA1MVoXDTE3MDQyNDIxNTkwMFowVTELMAkGA1UEBhMCTk8xGDAWBgNVBAoMD1BPU1RFTiBOT1JHRSBBUzEYMBYGA1UEAwwPUE9TVEVOIE5PUkdFIEFTMRIwEAYDVQQFEwk5ODQ2NjExODUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCLCxU4oBhtGmJxXZWbdWdzO2uA3eRNW/kPdddL1HYl1iXLV/g+H2Q0ELadWLggkS+1kOd8/jKxEN++biMmmDqqCWbzNdmEd1j4lctSlH6M7tt0ywmXIYdZMz5kxcLAMNXsaqnPdikI9uPJZQEL3Kc8hXhXISvpzP7gYOvKHg41uCxu1xCZQOM6pTlNbxemBYqvES4fRh2xvB9aMjwkB4Nz8jrIsyoPI89i05OmGMkI5BPZt8NTa40Yf3yU+SQECW0GWalB5cxaTMeB01tqslUzBJPV3cQx+AhtQG4hkOhQnAMDJramSPVtwbEnqOjQ+lyNmg5GQ4FJO02ApKJTZDTHAgMBAAGjggHCMIIBvjAJBgNVHRMEAjAAMB8GA1UdIwQYMBaAFD+u9XgLkqNwIDVfWvr3JKBSAfBBMB0GA1UdDgQWBBQ1gsJfVC7KYGiWVLP7ZwzppyVYTTAOBgNVHQ8BAf8EBAMCBLAwFgYDVR0gBA8wDTALBglghEIBGgEAAwIwgbsGA1UdHwSBszCBsDA3oDWgM4YxaHR0cDovL2NybC50ZXN0NC5idXlwYXNzLm5vL2NybC9CUENsYXNzM1Q0Q0EzLmNybDB1oHOgcYZvbGRhcDovL2xkYXAudGVzdDQuYnV5cGFzcy5uby9kYz1CdXlwYXNzLGRjPU5PLENOPUJ1eXBhc3MlMjBDbGFzcyUyMDMlMjBUZXN0NCUyMENBJTIwMz9jZXJ0aWZpY2F0ZVJldm9jYXRpb25MaXN0MIGKBggrBgEFBQcBAQR+MHwwOwYIKwYBBQUHMAGGL2h0dHA6Ly9vY3NwLnRlc3Q0LmJ1eXBhc3Mubm8vb2NzcC9CUENsYXNzM1Q0Q0EzMD0GCCsGAQUFBzAChjFodHRwOi8vY3J0LnRlc3Q0LmJ1eXBhc3Mubm8vY3J0L0JQQ2xhc3MzVDRDQTMuY2VyMA0GCSqGSIb3DQEBCwUAA4IBAQCe67UOZ/VSwcH2ov1cOSaWslL7JNfqhyNZWGpfgX1c0Gh+KkO3eVkMSozpgX6M4eeWBWJGELMiVN1LhNaGxBU9TBMdeQ3SqK219W6DXRJ2ycBtaVwQ26V5tWKRN4UlRovYYiY+nMLx9VrLOD4uoP6fm9GE5Fj0vSMMPvOEXi0NsN+8MUm3HWoBeUCLyFpe7/EPsS/Wud5bb0as/E2zIztRodxfNsoiXNvWaP2ZiPWFunIjK1H/8EcktEW1paiPd8AZek/QQoG0MKPfPIJuqH+WJU3a8J8epMDyVfaek+4+l9XOeKwVXNSOP/JSwgpOJNzTdaDOM+uVuk75n2191Fd7");
-
-            Assert.AreEqual(new X509Certificate2(certificateData), p.X509Sertifikat);
         }
+
+        [TestMethod]
+        public void HentPersonidentifikatorSuksess()
+        {
+            Assert.AreEqual("08077000292", _person.Personidentifikator);
+        }
+
+        [TestMethod]
+        public void HentReservasjonOgStatusSuksess()
+        {
+            Assert.IsFalse((bool)_person.Reservasjon);
+            Assert.AreEqual(Tilstand.AKTIV, _person.Status);
+        }
+
+        [TestMethod]
+        public void HentMobiltelefonnummerOgMetadataSuksess()
+        {
+            Assert.AreEqual("+4740485641", _person.Kontaktinformasjon.Mobiltelefonnummer.Nummer);
+            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 19), _person.Kontaktinformasjon.Mobiltelefonnummer.SistOppdatert);
+            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 19), _person.Kontaktinformasjon.Mobiltelefonnummer.SistVerifisert);
+        }
+
+        [TestMethod]
+        public void HentEpostOgMetadataSuksess()
+        {
+            Assert.AreEqual("extfjo@vegvesen.no", _person.Kontaktinformasjon.Epostadresse.Epost);
+            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 30), _person.Kontaktinformasjon.Epostadresse.SistOppdatert);
+            Assert.AreEqual(new DateTime(2015, 04, 14, 14, 35, 30), _person.Kontaktinformasjon.Epostadresse.SistVerifisert);
+        }
+
+        [TestMethod]
+        public void HentSikkerDigitalPostAdresseSuksess()
+        {
+            Assert.AreEqual("id.porten.testuser#2346", _person.SikkerDigitalPostAdresse.Postkasseadresse);
+            Assert.AreEqual("984661185", _person.SikkerDigitalPostAdresse.PostkasseleverandørAdresse);
+        }
+
+        [TestMethod]
+        public void HentSertifikatSuksess()
+        {
+            var certificateData = Convert.FromBase64String(@"MIIE7jCCA9agAwIBAgIKGBZrmEgzTHzeJjANBgkqhkiG9w0BAQsFADBRMQswCQYDVQQGEwJOTzEdMBsGA1UECgwUQnV5cGFzcyBBUy05ODMxNjMzMjcxIzAhBgNVBAMMGkJ1eXBhc3MgQ2xhc3MgMyBUZXN0NCBDQSAzMB4XDTE0MDQyNDEyMzA1MVoXDTE3MDQyNDIxNTkwMFowVTELMAkGA1UEBhMCTk8xGDAWBgNVBAoMD1BPU1RFTiBOT1JHRSBBUzEYMBYGA1UEAwwPUE9TVEVOIE5PUkdFIEFTMRIwEAYDVQQFEwk5ODQ2NjExODUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCLCxU4oBhtGmJxXZWbdWdzO2uA3eRNW/kPdddL1HYl1iXLV/g+H2Q0ELadWLggkS+1kOd8/jKxEN++biMmmDqqCWbzNdmEd1j4lctSlH6M7tt0ywmXIYdZMz5kxcLAMNXsaqnPdikI9uPJZQEL3Kc8hXhXISvpzP7gYOvKHg41uCxu1xCZQOM6pTlNbxemBYqvES4fRh2xvB9aMjwkB4Nz8jrIsyoPI89i05OmGMkI5BPZt8NTa40Yf3yU+SQECW0GWalB5cxaTMeB01tqslUzBJPV3cQx+AhtQG4hkOhQnAMDJramSPVtwbEnqOjQ+lyNmg5GQ4FJO02ApKJTZDTHAgMBAAGjggHCMIIBvjAJBgNVHRMEAjAAMB8GA1UdIwQYMBaAFD+u9XgLkqNwIDVfWvr3JKBSAfBBMB0GA1UdDgQWBBQ1gsJfVC7KYGiWVLP7ZwzppyVYTTAOBgNVHQ8BAf8EBAMCBLAwFgYDVR0gBA8wDTALBglghEIBGgEAAwIwgbsGA1UdHwSBszCBsDA3oDWgM4YxaHR0cDovL2NybC50ZXN0NC5idXlwYXNzLm5vL2NybC9CUENsYXNzM1Q0Q0EzLmNybDB1oHOgcYZvbGRhcDovL2xkYXAudGVzdDQuYnV5cGFzcy5uby9kYz1CdXlwYXNzLGRjPU5PLENOPUJ1eXBhc3MlMjBDbGFzcyUyMDMlMjBUZXN0NCUyMENBJTIwMz9jZXJ0aWZpY2F0ZVJldm9jYXRpb25MaXN0MIGKBggrBgEFBQcBAQR+MHwwOwYIKwYBBQUHMAGGL2h0dHA6Ly9vY3NwLnRlc3Q0LmJ1eXBhc3Mubm8vb2NzcC9CUENsYXNzM1Q0Q0EzMD0GCCsGAQUFBzAChjFodHRwOi8vY3J0LnRlc3Q0LmJ1eXBhc3Mubm8vY3J0L0JQQ2xhc3MzVDRDQTMuY2VyMA0GCSqGSIb3DQEBCwUAA4IBAQCe67UOZ/VSwcH2ov1cOSaWslL7JNfqhyNZWGpfgX1c0Gh+KkO3eVkMSozpgX6M4eeWBWJGELMiVN1LhNaGxBU9TBMdeQ3SqK219W6DXRJ2ycBtaVwQ26V5tWKRN4UlRovYYiY+nMLx9VrLOD4uoP6fm9GE5Fj0vSMMPvOEXi0NsN+8MUm3HWoBeUCLyFpe7/EPsS/Wud5bb0as/E2zIztRodxfNsoiXNvWaP2ZiPWFunIjK1H/8EcktEW1paiPd8AZek/QQoG0MKPfPIJuqH+WJU3a8J8epMDyVfaek+4+l9XOeKwVXNSOP/JSwgpOJNzTdaDOM+uVuk75n2191Fd7");
+            Assert.AreEqual(new X509Certificate2(certificateData), _person.X509Sertifikat);
+        }
+
+        [TestMethod]
+        public void Testmethod()
+        {
+            
+
+
+            
+        } 
 
         private static string PersonXml()
         {
