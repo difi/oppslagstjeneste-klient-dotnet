94c6f0d35e2297fb32ab4bd903e325d3844aaa81
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-28 15:28:24 +0200

    Lagt til integrasjonstester som kun sjekker at svar er ok

--- Difi.Oppslagstjeneste.Klient.Testklient/Program.cs
+++ Difi.Oppslagstjeneste.Klient.Testklient/Program.cs
@@ -17,20 +17,11 @@ namespace Difi.Oppslagstjeneste.Klient.Testklient
             
             Logging.Initialize(konfig);
             Logging.Log(TraceEventType.Information, "> Starter program!");
-
-            var storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            storeMy.Open(OpenFlags.ReadOnly);
-
-            var avsenderSertifikat = CertificateUtility.SenderCertificate("B0CB922214D11E8CE993838DB4C6D04C0C0970B8", Language.Norwegian);
-            var valideringssertifikat =
-                CertificateUtility.ReceiverCertificate("a4 7d 57 ea f6 9b 62 77 10 fa 0d 06 ec 77 50 0b af 71 c4 32",
-                    Language.Norwegian);
-
-            ResourceUtility rr = new ResourceUtility("Difi.Oppslagstjeneste.Klient.Testklient.Resources");
-            var certBytes = rr.ReadAllBytes(true, "cert.idporten-ver2.difi.no-v2.crt");
-            var valideringsSertifikat = new X509Certificate2(certBytes);
             
-            OppslagstjenesteKlient register = new OppslagstjenesteKlient(avsenderSertifikat, valideringsSertifikat, konfig);
+            var avsenderSertifikat = CertificateUtility.SenderCertificate("B0CB922214D11E8CE993838DB4C6D04C0C0970B8", Language.Norwegian);
+            var valideringssertifikat = CertificateUtility.ReceiverCertificate("a4 7d 57 ea f6 9b 62 77 10 fa 0d 06 ec 77 50 0b af 71 c4 32", Language.Norwegian);
+            
+            OppslagstjenesteKlient register = new OppslagstjenesteKlient(avsenderSertifikat, valideringssertifikat, konfig);
 
             var endringer = register.HentEndringer(886730, 
                 Informasjonsbehov.Kontaktinfo | 
