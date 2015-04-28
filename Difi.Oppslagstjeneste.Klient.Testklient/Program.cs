717ec83cbd43c8f12e7be974889dda589b757cf9
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-28 15:00:43 +0200

    Fjernet valideringssertifikat fra ressurser

--- Difi.Oppslagstjeneste.Klient.Testklient/Program.cs
+++ Difi.Oppslagstjeneste.Klient.Testklient/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using ApiClientShared;
+using ApiClientShared.Enums;
 
 namespace Difi.Oppslagstjeneste.Klient.Testklient
 {
@@ -20,7 +21,10 @@ namespace Difi.Oppslagstjeneste.Klient.Testklient
             var storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
             storeMy.Open(OpenFlags.ReadOnly);
 
-            var avsenderSertifikat = storeMy.Certificates.Find(X509FindType.FindByThumbprint, "B0CB922214D11E8CE993838DB4C6D04C0C0970B8", true)[0];
+            var avsenderSertifikat = CertificateUtility.SenderCertificate("B0CB922214D11E8CE993838DB4C6D04C0C0970B8", Language.Norwegian);
+            var valideringssertifikat =
+                CertificateUtility.ReceiverCertificate("a4 7d 57 ea f6 9b 62 77 10 fa 0d 06 ec 77 50 0b af 71 c4 32",
+                    Language.Norwegian);
 
             ResourceUtility rr = new ResourceUtility("Difi.Oppslagstjeneste.Klient.Testklient.Resources");
             var certBytes = rr.ReadAllBytes(true, "cert.idporten-ver2.difi.no-v2.crt");
