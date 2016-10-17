6dff648209f5a53a702dfd932cf730234763293c
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-10-17 10:52:33 +0200

    Bumper versjon for sertifikathenting fra fra LocalMachine
    - Sertifikater kan nå også hentes fra LocalMachine. Da må versjon bumpes

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Reflection;
 [assembly: AssemblyTrademark("Direktoratet for forvaltning og IKT (Difi)")]
 [assembly: AssemblyProduct("Difi Opppslagstjeneste Klient")]
 [assembly: AssemblyDescription("Klientbibliotek for integrasjon mot Oppslagstjenesten for kontakt og reservasjonregisteret")]
-[assembly: AssemblyVersion("5.4.0.*")]
+[assembly: AssemblyVersion("5.5.0.*")]
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
-[assembly: AssemblyFileVersion("5.4.0.*")]
+[assembly: AssemblyFileVersion("5.5.0.*")]
 #pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
 [assembly: AssemblyCopyright("© 2015-2016 Direktoratet for forvaltning og IKT (Difi)")]
 [assembly: AssemblyCulture("")]
